b8cec2d3a4c8fbb4e87d58a2dd89998d3875d926
MaxinAIAvto <avto.bolashvili@maxinai.com>
2022-05-30 16:47:06 +0400

    Fix

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -39,6 +39,21 @@ const Timer = () => {
    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
+    const calculateTimeLeft = () => {
+      const difference = new Date(2032, 11, 31) - new Date();
+      let timeLeft = {};
+
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60)
+        };
+      }
+      addZeros(timeLeft);
+      return timeLeft;
+    };
     setInterval(() => {
       setTimeLeft(calculateTimeLeft())
       audio.play();
